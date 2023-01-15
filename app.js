b2b5ea85d7cd63c1b5c41e7936e2b34a11f263b0
Safak Bahce <safakbahce@gmail.com>
2023-01-15 10:50:04 +0300

    add features

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, { cors: { origin: '*' } });
 
+var onlineCount = 0;
+var users = [];
+
 io.on('connection', (socket) => {
+
+    onlineCount++;
+    io.emit('onlineCount', onlineCount);
+
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected  ' + socket.name);
+        onlineCount--;
+        io.emit('onlineCount', onlineCount);
+        users = users.filter((item) => item !== socket.name);
+        io.emit('new connection', users);
     });
+
     socket.on('new connection', (req) => {
+        socket.name = req.id;
         console.log('data: ' + req.data + ' id: ' + req.id);
+        users.push(req.id);
+        io.emit('new connection', users);
     });
+
     socket.on('chat message', (msg, userId) => {
         console.log('message: ' + msg);
         io.emit('chat message', msg, userId);
